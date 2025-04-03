4aa48bcf4aab7887426f57acbfc17d1c065e3976
soctopus2327 <152524824+soctopus2327@users.noreply.github.com>
2025-04-04 02:43:27 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get install -y graphviz
 pip install -r requirements.txt
 python -m spacy download en_core_web_sm
 python -c "import nltk; nltk.download('punkt'); nltk.download('averaged_perceptron_tagger')"
