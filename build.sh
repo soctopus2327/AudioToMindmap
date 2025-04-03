9a39929adda2b68665e4fb61a2b6a527ff550a60
soctopus2327 <152524824+soctopus2327@users.noreply.github.com>
2025-04-04 01:47:58 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pip install -r requirements.txt
 python -m spacy download en_core_web_sm
-python -c "import nltk; nltk.download('punkt')"
+python -c "import nltk; nltk.download('punkt'); nltk.download('averaged_perceptron_tagger')"
