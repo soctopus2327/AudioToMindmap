201db754a975264f60411b118dbb5b15d6a893ee
soctopus2327 <152524824+soctopus2327@users.noreply.github.com>
2025-04-04 10:20:35 +0530

    Create run.py

---
+++ run.py
@@ -0,0 +1,10 @@
+import nltk
+nltk.download('punkt', quiet=True)
+
+import spacy
+try:
+    nlp = spacy.load("en_core_web_sm")
+except:
+    import subprocess
+    subprocess.run(["python", "-m", "spacy", "download", "en_core_web_sm"])
+    nlp = spacy.load("en_core_web_sm")
