14275951ee592d9684d02f30e2ff9bbc015c676e
soctopus2327 <152524824+soctopus2327@users.noreply.github.com>
2025-04-04 10:15:44 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Updated build.sh
-pip install --upgrade pip
+pip install --upgrade "pip<23.0"
+pip install rich==13.7.0
 pip install -r requirements.txt
 python -m spacy download en_core_web_sm
 python -c "import nltk; nltk.download('punkt')"
